930ff5c3ffe2f6596e8485bd92861ef9616183a7
Charith Pulaganti <chakri9701@gmail.com>
2026-09-08 17:30:08 +0530

    ✅ collections.Counter() | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-08

---
+++ hackerrank/medium/collections-counter/solution.py
@@ -0,0 +1,13 @@
+from collections import Counter
+n = int(input())
+shoes = Counter(map(int, input().split()))
+customers = int(input())
+total = 0
+
+for _ in range(customers):
+    size, price = map(int, input().split())
+    if shoes[size] > 0:
+        total += price
+        shoes[size] -= 1
+        
+print(total)
